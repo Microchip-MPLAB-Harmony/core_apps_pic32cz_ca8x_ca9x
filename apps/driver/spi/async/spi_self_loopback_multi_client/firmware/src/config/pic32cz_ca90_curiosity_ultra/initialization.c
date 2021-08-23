8d770072d6d995221c9dea9f0865558dd4f11015
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-23 10:35:14 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_self_loopback_multi_client_pic32cz_ca90_curiosity_ultra
    MH3-55626
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32cz_ca90_curiosity_ultra/initialization.c
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32cz_ca90_curiosity_ultra/initialization.c
@@ -81,7 +81,7 @@ const DRV_SPI_PLIB_INTERFACE drvSPI0PlibAPI = {
     .writeRead = (DRV_SPI_PLIB_WRITE_READ)SERCOM2_SPI_WriteRead,
 
     /* SPI PLIB Transfer Status function */
-    .isBusy = (DRV_SPI_PLIB_IS_BUSY)SERCOM2_SPI_IsBusy,
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SERCOM2_SPI_IsTransmitterBusy,
 
     /* SPI PLIB Callback Register */
     .callbackRegister = (DRV_SPI_PLIB_CALLBACK_REGISTER)SERCOM2_SPI_CallbackRegister,
@@ -120,11 +120,6 @@ const DRV_SPI_INIT drvSPI0InitData =
     /* SPI Client Objects Pool */
     .clientObjPool = (uintptr_t)&drvSPI0ClientObjPool[0],
 
-    /* DMA Channel for Transmit */
-    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
-
-    /* DMA Channel for Receive */
-    .dmaChannelReceive  = SYS_DMA_CHANNEL_NONE,
 
     /* SPI Queue Size */
     .transferObjPoolSize = DRV_SPI_QUEUE_SIZE_IDX0,
@@ -192,6 +187,8 @@ void SYS_Initialize ( void* data )
 
 
     SERCOM2_SPI_Initialize();
+
+    EVSYS_Initialize();
 
 	BSP_Initialize();
 
