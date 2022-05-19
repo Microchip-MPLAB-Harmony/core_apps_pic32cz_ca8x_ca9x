27f0b98fb4225d983ec9ff91825f944f05ec4526
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-19 11:58:27 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_async_i2c_multi_slave_pic32cz_ca90_curiosity_ultra
    MH3-64505
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/pic32cz_ca90_curiosity_ultra/definitions.h
+++ apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/pic32cz_ca90_curiosity_ultra/definitions.h
@@ -59,11 +59,11 @@
 #include "system/time/sys_time.h"
 #include "system/console/sys_console.h"
 #include "system/console/src/sys_console_uart_definitions.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "bsp/bsp.h"
 #include "app_i2c_eeprom.h"
 #include "app_i2c_temp_sensor.h"
 
