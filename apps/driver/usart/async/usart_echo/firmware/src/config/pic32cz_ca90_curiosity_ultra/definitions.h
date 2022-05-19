3611cf58450451460ba768839b4f9c9aae790c9c
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-19 12:05:23 +0530

    [DRV_USART] Regenerated demo for driver_usart_async_usart_echo_pic32cz_ca90_curiosity_ultra
    MH3-64510
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/async/usart_echo/firmware/src/config/pic32cz_ca90_curiosity_ultra/definitions.h
+++ apps/driver/usart/async/usart_echo/firmware/src/config/pic32cz_ca90_curiosity_ultra/definitions.h
@@ -54,12 +54,12 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/sercom/usart/plib_sercom5_usart.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "system/dma/sys_dma.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "bsp/bsp.h"
 #include "app.h"
 
 
