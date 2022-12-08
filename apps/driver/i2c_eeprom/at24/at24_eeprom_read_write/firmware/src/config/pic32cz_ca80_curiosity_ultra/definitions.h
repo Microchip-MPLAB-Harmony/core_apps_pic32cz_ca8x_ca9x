a0925038192517b809c1b698a232c6c7243d6e9b
Syam Nair <syam.nair@microchip.com>
2022-12-09 01:19:36 +0530

    [DRV_I2C_EEPROM] Regenerated demo for driver_i2c_eeprom_at24_at24_eeprom_read_write_pic32cz_ca80_curiosity_ultra
    MH3-73383
    
    Signed-off-by: Syam Nair <syam.nair@microchip.com>

--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
@@ -54,12 +54,12 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/sercom/i2c_master/plib_sercom5_i2c_master.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "bsp/bsp.h"
 #include "app.h"
 
 
