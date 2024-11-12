2b02d1c74fad6861dd165f3f7fc873ea20ef7708
Vishal Gandhi <vishal.gandhi@microchip.com>
2024-11-12 11:33:40 +0530

    [SYS_] Regenerated demo for system__time__sys_time_multiclient__firmware__pic32cz_ca80_curiosity_ultra.X
    MH3-103028

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
@@ -54,8 +54,8 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "bsp/bsp.h"
 #include "peripheral/rtc/plib_rtc.h"
+#include "bsp/bsp.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
@@ -74,13 +74,13 @@ extern "C" {
 // DOM-IGNORE-END
 
 /* Device Information */
-#define DEVICE_NAME			 "PIC32CZ8110CA80208"
-#define DEVICE_ARCH			 "CORTEX-M7"
-#define DEVICE_FAMILY		 "PIC32C"
-#define DEVICE_SERIES		 "PIC32CZCA80"
+#define DEVICE_NAME          "PIC32CZ8110CA80208"
+#define DEVICE_ARCH          "CORTEX-M7"
+#define DEVICE_FAMILY        "PIC32C"
+#define DEVICE_SERIES        "PIC32CZCA80"
 
 /* CPU clock frequency */
-#define CPU_CLOCK_FREQUENCY 300000000
+#define CPU_CLOCK_FREQUENCY 300000000U
 
 // *****************************************************************************
 // *****************************************************************************
