c8624d1c1c39b60c7ff7388a5820eb8f191074b5
Vishal Gandhi <vishal.gandhi@microchip.com>
2024-11-12 11:33:34 +0530

    [SYS_FS] Regenerated demo for fs__sdspi_fat__firmware__pic32cz_ca80_curiosity_ultra.X
    MH3-103020

--- apps/fs/sdspi_fat/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
+++ apps/fs/sdspi_fat/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
@@ -71,28 +71,34 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
+   PORT_REGS->GROUP[0].PORT_PINCFG[19] = 0x0U;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x0U;
 
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x600000U;
    PORT_REGS->GROUP[1].PORT_OUT = 0x1600000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[1] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PINCFG[21] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PINCFG[22] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x6U;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[0] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PMUX[11] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x0U;
 
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x4000U;
-   PORT_REGS->GROUP[2].PORT_OUT = 0x4000U;
+   PORT_REGS->GROUP[2].PORT_OUT = 0x804000U;
+   PORT_REGS->GROUP[2].PORT_PINCFG[6] = 0x0U;
    PORT_REGS->GROUP[2].PORT_PINCFG[12] = 0x1U;
    PORT_REGS->GROUP[2].PORT_PINCFG[13] = 0x1U;
    PORT_REGS->GROUP[2].PORT_PINCFG[14] = 0x0U;
    PORT_REGS->GROUP[2].PORT_PINCFG[15] = 0x1U;
    PORT_REGS->GROUP[2].PORT_PINCFG[23] = 0x6U;
 
+   PORT_REGS->GROUP[2].PORT_PMUX[3] = 0x0U;
    PORT_REGS->GROUP[2].PORT_PMUX[6] = 0x33U;
    PORT_REGS->GROUP[2].PORT_PMUX[7] = 0x30U;
    PORT_REGS->GROUP[2].PORT_PMUX[11] = 0x0U;
@@ -278,6 +284,14 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask)
 void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 {
     ((port_group_registers_t*)group)->PORT_DIRCLR = mask;
+    
+    for(uint32_t i = 0U; i < 32U; i++)
+    {
+        if((mask & ((uint32_t)1U << i)) != 0U)
+        {
+            ((port_group_registers_t*)group)->PORT_PINCFG[i] |= PORT_PINCFG_INEN_Msk;
+        }
+    }
 }
 
 // *****************************************************************************
