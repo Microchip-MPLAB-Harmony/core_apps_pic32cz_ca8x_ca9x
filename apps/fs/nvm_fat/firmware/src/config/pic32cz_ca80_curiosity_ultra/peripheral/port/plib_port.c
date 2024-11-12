533cb5225acd8b343835c948e35a0551131ef555
Vishal Gandhi <vishal.gandhi@microchip.com>
2024-11-12 11:33:32 +0530

    [SYS_FS] Regenerated demo for fs__nvm_fat__firmware__pic32cz_ca80_curiosity_ultra.X
    MH3-103016

--- apps/fs/nvm_fat/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
+++ apps/fs/nvm_fat/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
@@ -76,13 +76,17 @@ void PORT_Initialize(void)
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x600000U;
    PORT_REGS->GROUP[1].PORT_OUT = 0x1600000U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[21] = 0x0U;
+   PORT_REGS->GROUP[1].PORT_PINCFG[22] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PINCFG[24] = 0x6U;
 
+   PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x0U;
+   PORT_REGS->GROUP[1].PORT_PMUX[11] = 0x0U;
    PORT_REGS->GROUP[1].PORT_PMUX[12] = 0x0U;
 
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_OUT = 0x800000U;
-   PORT_REGS->GROUP[2].PORT_PINCFG[23] = 0x4U;
+   PORT_REGS->GROUP[2].PORT_PINCFG[23] = 0x6U;
 
    PORT_REGS->GROUP[2].PORT_PMUX[11] = 0x0U;
 
@@ -267,6 +271,14 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask)
 void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 {
     ((port_group_registers_t*)group)->PORT_DIRCLR = mask;
+    
+    for(uint32_t i = 0U; i < 32U; i++)
+    {
+        if((mask & ((uint32_t)1U << i)) != 0U)
+        {
+            ((port_group_registers_t*)group)->PORT_PINCFG[i] |= PORT_PINCFG_INEN_Msk;
+        }
+    }
 }
 
 // *****************************************************************************
