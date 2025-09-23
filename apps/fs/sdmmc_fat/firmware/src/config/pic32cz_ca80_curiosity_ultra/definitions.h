4edd6f22682534adedda8c1cba5c9cf912b55753
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 10:05:09 +0000

    [APP] Regenerated demo for fs__sdmmc_fat__firmware
    MH3C-328

--- apps/fs/sdmmc_fat/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
+++ apps/fs/sdmmc_fat/firmware/src/config/pic32cz_ca80_curiosity_ultra/definitions.h
@@ -14,7 +14,7 @@
 
 //DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -49,24 +49,24 @@
 #include <stddef.h>
 #include <stdbool.h>
 #include "peripheral/evsys/plib_evsys.h"
+#include "driver/sdmmc/drv_sdmmc.h"
 #include "system/fs/sys_fs.h"
 #include "system/fs/sys_fs_media_manager.h"
 #include "system/fs/sys_fs_fat_interface.h"
 #include "system/fs/fat_fs/file_system/ff.h"
 #include "system/fs/fat_fs/file_system/ffconf.h"
 #include "system/fs/fat_fs/hardware_access/diskio.h"
-#include "driver/sdmmc/drv_sdmmc.h"
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/systick/plib_systick.h"
 #include "peripheral/sdmmc/plib_sdmmc1.h"
 #include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
