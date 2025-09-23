22e9001db8cb724ef296b02817e8117baef457e4
Syam Nair <Syam.Nair@microchip.com>
2025-09-23 10:03:43 +0000

    [APP] Regenerated demo for system__time__sys_time_multiclient__firmware
    MH3C-328

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32cz_ca80_curiosity_ultra/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32cz_ca80_curiosity_ultra/initialization.c
@@ -15,7 +15,7 @@
 
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2025 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -380,9 +380,9 @@ void SYS_Initialize ( void* data )
 
     EVSYS_Initialize();
 
+	BSP_Initialize();
     RTC_Initialize();
 
-	BSP_Initialize();
 
     /* MISRAC 2012 deviation block start */
     /* Following MISRA-C rules deviated in this block  */
